275b8f0c640fc52dec881b240651e119aeeee555
Zheng Wang <wzheng2013@gmail.com>
2020-01-16 16:12:37 -0500

    driving code

--- 2020RobotCode/src/main/java/frc/robot/Robot.java
+++ 2020RobotCode/src/main/java/frc/robot/Robot.java
@@ -1,26 +1,44 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends TimedRobot {
 
-  // Hardware
+  // joystick, motor, network table
 
   Joystick joystick;
-
-  // motor
+  TalonSRX leftFront;
+  TalonSRX leftBack;
+  TalonSRX rightFront;
+  TalonSRX rightBack;
+  double x;
+  double y;
 
   @Override
   public void robotInit() {
     joystick = new Joystick(0);
+
+    leftFront = new TalonSRX(0);
+    leftBack = new TalonSRX(1);
+    rightFront = new TalonSRX(2);
+    rightBack = new TalonSRX(3);
+
     // network tables here
     // pid values config here
   }
 
   public void commonLoop() {
     // the number starts at 1
-    boolean buttonNumber = joystick.getRawButton(1);
+    x = joystick.getX();
+    y = -joystick.getY();
+
+    leftFront.set(ControlMode.PercentOutput, 0, DemandType.ArbitraryFeedForward, 1);
   }
 
   public void autoLoop() {
